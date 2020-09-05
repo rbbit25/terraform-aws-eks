9a012cdac6b82906490ee039d56a38a6f41e190d
William MR <william.munozr@gmail.com>
2020-09-05 11:50:46 -0500

    Fixing some minor issues

--- datasource.tf
+++ datasource.tf
@@ -8,11 +8,11 @@ data "aws_availability_zones" "available" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks-wordpress.cluster_id
+  name = module.eks-cluster.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks-wordpress.cluster_id
+  name = module.eks-cluster.cluster_id
 }
 
 provider "kubernetes" {
