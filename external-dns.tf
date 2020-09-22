35c561274fe38c060636e10592f3431c42b04b22
William MR <william.munozr@gmail.com>
2020-09-21 19:30:48 -0500

    External DNS Configuration

--- external-dns.tf
+++ external-dns.tf
@@ -48,114 +48,44 @@ resource "kubernetes_service_account" "external_dns" {
   automount_service_account_token = true
 }
 
-resource "kubernetes_cluster_role" "external_dns" {
-  metadata {
-    name = "external-dns"
-  }
-
-  rule {
-    api_groups  = [""]
-    resources   = ["services", "pods", "nodes"]
-    verbs       = ["get", "list", "watch"]
-  }
-
-  rule {
-    api_groups  = ["extensions", "networking.k8s.io"]
-    resources   = ["ingresses"]
-    verbs       = ["get", "list", "watch"]
-  }
-
-  rule {
-    api_groups  = ["networking.istio.io"]
-    resources   = ["gateways"]
-    verbs       = ["get", "list", "watch"]
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "external_dns" {
-  metadata {
-    name = "external-dns"
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind = "ClusterRole"
-    name = kubernetes_cluster_role.external_dns.metadata.0.name
-  }
-
-  subject {
-    kind = "ServiceAccount"
-    name = kubernetes_service_account.external_dns.metadata.0.name
-    namespace = kubernetes_service_account.external_dns.metadata.0.namespace
-  }
-}
-
-# resource "helm_release" "external_dns" {
-#   name       = "external-dns"
-#   namespace  = kubernetes_service_account.external_dns.metadata.0.namespace
-#   wait       = true
-#   repository = data.helm_repository.bitnami.metadata.0.name
-#   chart      = "external-dns"
-#   version    = var.external_dns_chart_version
-
-#   set {
-#     name  = "rbac.create"
-#     value = false
+# resource "kubernetes_cluster_role" "external_dns" {
+#   metadata {
+#     name = "external-dns"
 #   }
 
-#   set {
-#     name  = "serviceAccount.create"
-#     value = false
+#   rule {
+#     api_groups  = [""]
+#     resources   = ["services", "pods", "nodes"]
+#     verbs       = ["get", "list", "watch"]
 #   }
 
-#   set {
-#     name  = "serviceAccount.name"
-#     value = kubernetes_service_account.external_dns.metadata.0.name
+#   rule {
+#     api_groups  = ["extensions", "networking.k8s.io"]
+#     resources   = ["ingresses"]
+#     verbs       = ["get", "list", "watch"]
 #   }
 
-#   set {
-#     name  = "rbac.pspEnabled"
-#     value = false
+#   rule {
+#     api_groups  = ["networking.istio.io"]
+#     resources   = ["gateways"]
+#     verbs       = ["get", "list", "watch"]
+#   }
+# }
+
+# resource "kubernetes_cluster_role_binding" "external_dns" {
+#   metadata {
+#     name = "external-dns"
 #   }
 
-#   set {
-#     name  = "name"
-#     value = "${var.eks_cluster_name}-external-dns"
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind = "ClusterRole"
+#     name = kubernetes_cluster_role.external_dns.metadata.0.name
 #   }
 
-#   set {
-#     name  = "provider"
-#     value = "aws"
-#   }
-
-#   set_string {
-#     name  = "policy"
-#     value = "sync"
-#   }
-
-#   set_string {
-#     name  = "logLevel"
-#     value = var.external_dns_chart_log_level
-#   }
-
-#   set {
-#     name  = "sources"
-#     value = "{ingress,service}"
-#   }
-
-#   set {
-#     name  = "domainFilters"
-#     value = "{${join(",", var.external_dns_domain_filters)}}"
-#   }
-
-#   set_string {
-#     name  = "aws.zoneType"
-#     value = var.external_dns_zoneType
-#   }
-
-#   set_string {
-#     name  = "aws.region"
-#     #value = var.aws_region
-#     value = lookup(var.region, var.environment)
+#   subject {
+#     kind = "ServiceAccount"
+#     name = kubernetes_service_account.external_dns.metadata.0.name
+#     namespace = kubernetes_service_account.external_dns.metadata.0.namespace
 #   }
 # }
