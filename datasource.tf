7d3984c2301243ce9be0c634cf58185839e1fb4d
William MR <william.munozr@gmail.com>
2020-09-22 09:00:41 -0500

    MySQL HA deployment with Helm

--- datasource.tf
+++ datasource.tf
@@ -15,23 +15,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks-cluster.cluster_id
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.9"
-}
-
-provider "helm" {
-  kubernetes {
-    host                    = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate  = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                   = data.aws_eks_cluster_auth.cluster.token
-    load_config_file        = false
-  }
-}
-
 data "helm_repository" "incubator" {
   name = "incubator"
   url  = "https://kubernetes-charts-incubator.storage.googleapis.com"
