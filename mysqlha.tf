b689a00d37ed767e1e38ac01e03accb93c30f107
William MR <william.munozr@gmail.com>
2020-09-22 10:08:59 -0500

    MySQL HA deployment with Helm

--- mysqlha.tf
+++ mysqlha.tf
@@ -7,4 +7,19 @@ resource "helm_release" "timedb" {
   name       = "timedb"
   repository = "https://kubernetes-charts-incubator.storage.googleapis.com"
   chart      = "mysqlha"
+
+  set {
+      name = "mysqlDatabase"
+      value = "timeoffdb"
+  }
+
+  set {
+      name = "mysqlUser"
+      value = "testuser"
+  }
+
+  set {
+      name = "mysqlPassword"
+      value = "testpassword"
+  }
 }
