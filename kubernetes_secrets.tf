3b3b9274325fd1cd247049f3b42160438f6af110
William MR <william.munozr@gmail.com>
2020-09-26 11:31:34 -0500

    RDS Endpoint

--- kubernetes_secrets.tf
+++ kubernetes_secrets.tf
@@ -3,21 +3,12 @@
     Develop By: William MR
 */
 
-locals {
-  db_creds = jsondecode(
-    data.aws_secretsmanager_secret_version.current.secret_string
-  )
-}
-
 resource "kubernetes_secret" "rds-db-credentials" {
     metadata {
         name = "rds-db-credentials"
     }
 
     data = {
-        db_name     = local.db_creds.db_name
-        db_username = local.db_creds.db_username
-        db_password = local.db_creds.db_password
         db_host     = module.db.this_db_instance_endpoint
         db_dialect  = "mysql"
     }
