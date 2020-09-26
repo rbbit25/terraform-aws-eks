3893a1510da3dd6163dbe613b11d63ead9ac0195
William MR <william.munozr@gmail.com>
2020-09-26 11:23:34 -0500

    Saving RDS DB Credentials in Kubernetes

--- kubernetes_secrets.tf
+++ kubernetes_secrets.tf
@@ -2,15 +2,22 @@
     EKS Secrets for RDS Database
     Develop By: William MR
 */
+
+locals {
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.current.secret_string
+  )
+}
+
 resource "kubernetes_secret" "rds-db-credentials" {
     metadata {
         name = "rds-db-credentials"
     }
 
     data = {
-        db_name     = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["db_name"]
-        db_username = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["db_username"]
-        db_password = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["db_password"]
+        db_name     = local.db_creds.db_name
+        db_username = local.db_creds.db_username
+        db_password = local.db_creds.db_password
         db_host     = module.db.this_db_instance_endpoint
         db_dialect  = "mysql"
     }
